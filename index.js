7cd89ffbd6ec12e99358b9b94d9969ad610f02cd
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-07 11:02:12 +0700

    xong

--- index.js
+++ index.js
@@ -10,9 +10,27 @@ app.use(express.static('public'));
 server.listen(3000, () => console.log('Server started'));
 app.get('/', (req, res) => res.render('home'));
 
+class User {
+    constructor(username, id) {
+        this.username = username;
+        this.id = id;
+    }
+}
+
+const arrUsers = [];
+
 io.on('connection', socket => {
     // setInterval(() => socket.emit('SERVER_SEND_MSG', Math.random()), 2000);
     socket.on(
         'CLIENT_SEND_MSG', 
-        message => io.emit('SERVER_SEND_MSG', message))
+        message => io.emit('SERVER_SEND_MSG', message));
+
+    socket.on('CLIENT_SIGN_UP', username => {
+        const isExist = arrUsers.some(e => e.username === username);
+        if (isExist) return socket.emit('XAC_NHAN_DANG_KY', false);
+        const user = new User(username, socket.id)
+        socket.emit('XAC_NHAN_DANG_KY', arrUsers);
+        arrUsers.push(user);
+        io.emit('NGUOI_DUNG_MOI', user);
+    });
 });
--- public/main.js
+++ public/main.js
@@ -7,7 +7,33 @@ socket.on(
     message => $('#ulMessages').append('<li>DA NHAN: ' + message + '</li>')
 );
 
+socket.on(
+    'XAC_NHAN_DANG_KY',
+    arrUser => {
+        if (!arrUser) return alert('Username da ton tai!');
+        $('#div-chat').show();
+        $('#div-sign-up').hide();
+        arrUser.forEach(user => $('#ulUsers').append(`<li>${user.username}</li>`));
+        socket.on(
+            'NGUOI_DUNG_MOI', 
+            user => $('#ulUsers').append(`<li>${user.username}</li>`)
+        );
+    }
+)
+
 $('#btnSend').click(() => {
     const message = $('#txtMessage').val();
     socket.emit('CLIENT_SEND_MSG', message);
 });
+
+$('#btnSignUp').click(() => {
+    const username = $('#txtUsername').val();
+    socket.emit('CLIENT_SIGN_UP', username);
+});
+/*
+    Neu da ton tai? -> Khong cho dang ky. alert()
+
+    Neu chua ton tai -> Cho dang ky.
+    an div-sign-up, show div-chat $('#div').show();
+    Ai da sign up -> ten phai co trong ulUsers
+*/
