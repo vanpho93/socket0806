a3fdcb632608607c467f2e78e6a4a473fdb975af
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-07 09:44:32 +0700

    chat

--- index.js
+++ index.js
@@ -10,4 +10,9 @@ app.use(express.static('public'));
 server.listen(3000, () => console.log('Server started'));
 app.get('/', (req, res) => res.render('home'));
 
-io.on('connection', socket => console.log(socket.id));
+io.on('connection', socket => {
+    // setInterval(() => socket.emit('SERVER_SEND_MSG', Math.random()), 2000);
+    socket.on(
+        'CLIENT_SEND_MSG', 
+        message => io.emit('SERVER_SEND_MSG', message))
+});
--- public/main.js
+++ public/main.js
@@ -1,1 +1,11 @@
 const socket = io();
+
+socket.on(
+    'SERVER_SEND_MSG', 
+    randomNumber => console.log('DA NHAN: ' + randomNumber)
+);
+
+$('#btnSend').click(() => {
+    const message = $('#txtMessage').val();
+    socket.emit('CLIENT_SEND_MSG', message);
+});
